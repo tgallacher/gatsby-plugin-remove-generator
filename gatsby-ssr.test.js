f7b0eed0a10e76867edc357695c97c241ac16a6c
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-08-17 21:22:33 +0100

    test: add initial test suite

---
+++ gatsby-ssr.test.js
@@ -0,0 +1,67 @@
+const { onPreRenderHTML } = require('./gatsby-ssr');
+
+it('keeps all non <meta> components', () => {
+  let result;
+  // mock component objects
+  const initialHeadComponents = [
+    { type: 'link', props: { href: '/#' } },
+    { type: 'script', props: { src: '/#' } },
+    { type: 'style', props: { id: 'emotional-styling' } },
+  ];
+  const getHeadComponents = jest.fn(() => initialHeadComponents);
+  const replaceHeadComponents = jest.fn(newComponents => {
+    result = newComponents;
+  });
+
+  onPreRenderHTML({ getHeadComponents, replaceHeadComponents });
+  expect(getHeadComponents).toHaveBeenCalled();
+  expect(replaceHeadComponents).toHaveBeenCalled();
+  expect(result).toEqual(initialHeadComponents);
+});
+
+it('keeps all non generator <meta> tags', () => {
+  let result;
+  // mock component objects
+  const initialHeadComponents = [
+    { type: 'meta', props: { charset: 'utf-8' } },
+    {
+      type: 'meta',
+      props: { name: 'viewport', content: 'width=device-width' },
+    },
+  ];
+  const getHeadComponents = jest.fn(() => initialHeadComponents);
+  const replaceHeadComponents = jest.fn(newComponents => {
+    result = newComponents;
+  });
+
+  onPreRenderHTML({ getHeadComponents, replaceHeadComponents });
+  expect(getHeadComponents).toHaveBeenCalled();
+  expect(replaceHeadComponents).toHaveBeenCalled();
+  expect(result).toEqual(initialHeadComponents);
+});
+
+it('removes the generator <meta> tag', () => {
+  let result;
+  // mock component objects
+  const initialHeadComponents = [
+    { type: 'link', props: { href: '/#' } },
+    {
+      type: 'meta',
+      props: { name: 'viewport', content: 'width=device-width' },
+    },
+  ];
+  const getHeadComponents = jest.fn(() =>
+    initialHeadComponents.concat({
+      type: 'meta',
+      props: { name: 'generator', content: 'Foobar 1.2.3' },
+    }),
+  );
+  const replaceHeadComponents = jest.fn(newComponents => {
+    result = newComponents;
+  });
+
+  onPreRenderHTML({ getHeadComponents, replaceHeadComponents });
+  expect(getHeadComponents).toHaveBeenCalled();
+  expect(replaceHeadComponents).toHaveBeenCalled();
+  expect(result).toEqual(initialHeadComponents);
+});
